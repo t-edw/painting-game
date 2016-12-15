cb7706791d36b46d68710d5077039db98bab074e
tedwards2018 <tedwards2018@student.amsacs.org>
2016-12-15 15:45:35 -0500

    Update calming.js

--- calming.js
+++ calming.js
@@ -106,7 +106,7 @@ function large(){
 	size=45;
 }
 function size(){
-	size=parseInt(document.getElementById("range").value,10);
+	size=document.getElementById("range").valueAsNumber;
 }
 // auto change image when compelte?
 // more precise instructions
